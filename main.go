aed5e4dc57b6b83040d33b6fdad22636fd909e3f
BradleyJrichardson <brdl.richardson@gmail.com>
2019-08-28 07:36:06 +1000

    .

--- main.go
+++ main.go
@@ -6,15 +6,48 @@ import (
 	"text/template"
 )
 
+type person struct {
+	Name string
+	Age  int
+}
+
+func (p person) SomeProcessing() int {
+	return 7
+}
+
+func (p person) AgeDbl() int {
+	return p.Age * 2
+}
+
+func (p person) TakesArg(x int) int {
+	return x * 2
+}
+
 var tpl *template.Template
 
 func init() {
-	tpl = template.Must(template.ParseGlob("nested-templates/*.gohtml"))
+	tpl = template.Must(template.ParseFiles("./templates/11-methods.gohtml"))
 }
 
 func main() {
-	err := tpl.ExecuteTemplate(os.Stdout, "index.gohtml", 42)
+
+	p := person{
+		Name: "Ian Fleming",
+		Age:  56,
+	}
+
+	err := tpl.Execute(os.Stdout, p)
 	if err != nil {
 		log.Fatalln(err)
 	}
 }
+
+// Generally speaking, best practice:
+// call functions in templates for formatting only; not processing or data access.
+
+// The main reasons you don't want to do any data processing in your template:
+// (1) separation of concerns
+// (2) if you're using a function more than once in a template,
+// the server needs to do the processing more than once.
+// (though the standard library might cache processing -
+// I've yet to dig into this - if you find the answer, let me know)
