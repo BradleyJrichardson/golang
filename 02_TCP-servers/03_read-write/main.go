3ab7d26e799607403e0bfe467eeede90b168e6f6
BradleyJrichardson <brdl.richardson@gmail.com>
2019-09-10 18:05:01 +1000

    .

---
+++ 02_TCP-servers/03_read-write/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"time"
+)
+
+func main() {
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer li.Close()
+
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		go handle(conn)
+	}
+}
+
+func handle(conn net.Conn) {
+	err := conn.SetDeadline(time.Now().Add(10 * time.Second))
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		if err != nil {
+			log.Println(err)
+		}
+		ln := scanner.Text()
+		fmt.Println(ln)
+		fmt.Fprintf(conn, "I heard you say: %s\n", ln)
+	}
+	defer conn.Close()
+
+	// we never get here
+	// we have an open stream connection
+	// how does the above reader know when it's done?
+	fmt.Println("Code got here.")
+}
