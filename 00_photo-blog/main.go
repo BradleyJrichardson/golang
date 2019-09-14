a721b167cd0b34db794016bf3b6d055bde7a14b2
BradleyJrichardson <brdl.richardson@gmail.com>
2019-09-14 17:36:57 +1000

    .

--- 00_photo-blog/main.go
+++ 00_photo-blog/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"html/template"
 	"net/http"
+	"strings"
 
 	uuid "github.com/satori/go.uuid"
 )
@@ -21,7 +22,9 @@ func main() {
 
 func index(w http.ResponseWriter, req *http.Request) {
 	c := getCookie(w, req)
-	tpl.ExecuteTemplate(w, "index.gohtml", c.Value)
+	c = appendValue(w, c)
+	xs := strings.Split(c.Value, "|")
+	tpl.ExecuteTemplate(w, "index.gohtml", xs)
 }
 
 func getCookie(w http.ResponseWriter, req *http.Request) *http.Cookie {
@@ -34,5 +37,26 @@ func getCookie(w http.ResponseWriter, req *http.Request) *http.Cookie {
 		}
 		http.SetCookie(w, c)
 	}
+	return c
+}
+
+func appendValue(w http.ResponseWriter, c *http.Cookie) *http.Cookie {
+	p1 := "disneyland.jpg"
+	p2 := "atbeach.jpg"
+	p3 := "hollywood.jpg"
+
+	s := c.Value
+	if !strings.Contains(s, p1) {
+		s += "|" + p1
+	}
+	if !strings.Contains(s, p2) {
+		s += "|" + p2
+	}
+	if !strings.Contains(s, p3) {
+		s += "|" + p3
+	}
+
+	c.Value = s
+	http.SetCookie(w, c)
 	return c
 }
